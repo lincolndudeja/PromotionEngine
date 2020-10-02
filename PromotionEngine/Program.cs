7bfcf9760a3eaa450ebf1cd834d3bd4fafc07a85
lincolndudeja <66211388+lincolndudeja@users.noreply.github.com>
2020-10-02 21:38:37 +0530

    Added the consumer logic.

--- PromotionEngine/Program.cs
+++ PromotionEngine/Program.cs
@@ -1,3 +1,5 @@
+using PromotionEngine.BusinessLogic.Models;
+using PromotionEngine.BusinessLogic.Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +12,30 @@ namespace PromotionEngineConsumer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the number of products");
+            int no =Convert.ToInt16( Console.ReadLine());
+            List<SkuProductCart> cartlist = new List<SkuProductCart>();
+            try
+            {
+                for (int i = 0; i < no; i++)
+                {
+                    SkuProductCart cart = new SkuProductCart();
+                    Console.WriteLine("Enter the product type");
+                    String productType = Console.ReadLine();
+                    Console.WriteLine("Enter the product Quantity");
+                    int quantity = Convert.ToInt16(Console.ReadLine());
+                    SkuProduct product = new SkuProduct(productType);
+                    cart.Quantity = quantity;
+                    cart.SkuProduct = product;
+                    cartlist.Add(cart);
+                }
+                int total = Wrapper.GetTotalAmount(cartlist);
+                Console.WriteLine("Total is: " + total);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Some Exception occur...please try again");
+            }
             Console.ReadKey();
         }
     }
