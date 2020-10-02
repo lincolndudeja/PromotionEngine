96c80ef2cb00fd177c33e45ba0c4c89f84440275
lincolndudeja <66211388+lincolndudeja@users.noreply.github.com>
2020-10-02 19:34:39 +0530

    Created the structre and added the projects

---
+++ PromotionEngine.BusinessLogic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PromotionEngine.BusinessLogic
+{
+    public class Class1
+    {
+    }
+}
---
+++ PromotionEngine.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PromotionEngine.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
