67cadd0d605b820921df6735bb3c081e103e18f8
lincolndudeja <66211388+lincolndudeja@users.noreply.github.com>
2020-10-02 20:20:37 +0530

    Added factory and Active rules for promotion

---
+++ PromotionEngine.BusinessLogic/Utility/ActiveRules.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.BusinessLogic.Utility
+{
+    public class ActiveRules
+    {
+        public static Dictionary<string, int> GetRulesDict()
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            dict.Add("3A", 130);
+            dict.Add("2B", 45);
+            dict.Add("CD", 30);
+            return dict;
+        }
+    }
+}
---
+++ PromotionEngine.BusinessLogic/Utility/Factory.cs
@@ -0,0 +1,22 @@
+﻿using PromotionEngine.BusinessLogic.Models;
+using PromotionEngine.BusinessLogic.PromotionServiceContracts;
+using PromotionEngine.BusinessLogic.PromotionServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.BusinessLogic.Utility
+{
+    public class Factory
+    {
+        public static IPromotionService GetPromotionService(PromotionType promotionType)
+        {
+            switch (promotionType)
+            {
+                case PromotionType.TypeA: return new PromotionServiceA();
+                case PromotionType.TypeB: return new PromotionServiceB();
+            }
+            return null;
+        }
+    }
+}
