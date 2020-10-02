9e3263cc69f8e3b3aa47fd420a18eb2191a9e216
lincolndudeja <66211388+lincolndudeja@users.noreply.github.com>
2020-10-02 20:56:53 +0530

    Added a wrapper class to calculate the combined total of the products

---
+++ PromotionEngine.BusinessLogic/Utility/Wrapper.cs
@@ -0,0 +1,25 @@
+﻿using PromotionEngine.BusinessLogic.Models;
+using PromotionEngine.BusinessLogic.PromotionServiceContracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PromotionEngine.BusinessLogic.Utility
+{
+    public class Wrapper
+    {
+        public static int GetTotalAmount(List<SkuProductCart> skuProductCarts)
+        {
+            int amount = 0;
+            IPromotionService promotionService;
+            foreach (PromotionType promotionType in Enum.GetValues(typeof(PromotionType)))
+            {
+                var typeAProducts = skuProductCarts.Where(x => x.SkuProduct.promotionType == promotionType).ToList();
+                promotionService = Factory.GetPromotionService(promotionType);
+                amount = amount + promotionService.GetTotalPrice(typeAProducts);
+            }
+            return amount;
+        }
+    }
+}
